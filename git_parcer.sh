5d57ba9de301f64dbd844c1fb325b066a7186173
Lana <svetlnovikova@gmail.com>
2018-08-14 21:24:11 +0300

    Update git_parcer.sh

--- git_parcer.sh
+++ git_parcer.sh
@@ -2,5 +2,5 @@ url="https://api.github.com/users"
 for i in $(cat list.txt); do
     content="$(curl -i "$url/$i")"
 #for s in $content:    
-    echo "$content" > output.txt
+    echo "$content" >> output.txt
 done
